122be99b276b635fd70ce4ed16599e699bf8b1ca
Victor Castro <victorcsa2002@gmail.com>
2023-08-12 19:29:28 -0300

    feat: add new endpoint

--- src/app.js
+++ src/app.js
@@ -10,4 +10,8 @@ app.get('/contact', (_, res) => {
   res.status(200).send('Contact page');
 });
 
+app.get('/about', (_, res) => {
+  res.status(200).send('About page');
+});
+
 module.exports = app;
